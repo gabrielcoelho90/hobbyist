550df80ce1f11c537119f2e4cfc42ae3c1ff07d4
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-06-14 15:58:29 -0300

    redis on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
 
+
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
@@ -66,6 +68,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
