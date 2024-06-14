72d0549c4c368732d915433dd2bb7138b9dca41d
Gabriel Chein <gabrielbcmuniz@gmail.com>
2024-06-14 02:46:06 -0300

    Finalized the form for filtering interest on searchpage

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -8,25 +8,6 @@ class InterestsController < ApplicationController
 
   def create
     respond_to do |format|
-      format.json {
-        @interests_arr = interest_params[:interestable_id]
-        @interests_arr.shift
-
-        @creation = ["Created interests for #{current_user.name}"]
-        @interestable_id_array = @interests_arr
-
-        @interests_arr.each do |interestable_id|
-          @interestable_id_array << interestable_id
-          subcommunity = Subcommunity.find(interestable_id)
-          interest = Interest.new user: current_user
-          interest.interestable = subcommunity
-          authorize interest
-          interest.save
-          @creation << subcommunity.name
-
-        end
-      }
-
       format.html {
         @interests_arr = interest_params[:interestable_id].compact_blank
 
@@ -46,15 +27,29 @@ class InterestsController < ApplicationController
             authorize interest
             interest.save
           end
-          # @interest = Interest.new
-          # @interest.user = current_user
 
-          # @communities = Community.all
-
-          # render :new, status: :unprocessable_entity
           redirect_to profile_path, notice: 'Interests succesfully saved!'
         end
       }
+
+      # format.json {
+      #   @interests_arr = interest_params[:interestable_id]
+      #   @interests_arr.shift
+
+      #   @creation = ["Created interests for #{current_user.name}"]
+      #   @interestable_id_array = @interests_arr
+
+      #   @interests_arr.each do |interestable_id|
+      #     @interestable_id_array << interestable_id
+      #     subcommunity = Subcommunity.find(interestable_id)
+      #     interest = Interest.new user: current_user
+      #     interest.interestable = subcommunity
+      #     authorize interest
+      #     interest.save
+      #     @creation << subcommunity.name
+
+      #   end
+      # }
     end
   end
 
