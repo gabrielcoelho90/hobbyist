dc259bb079f8d99760638aef00751ee705084964
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-06-17 10:57:58 -0300

    added new addresses and fixed some associations

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,14 +1,5 @@
 class Community < ApplicationRecord
   has_many :subcommunities, dependent: :destroy
-  has_many :interests, as: :interestable
-
-  # include PgSearch::Model
-  # pg_search_scope :global_search,
-  #   against: [:name],
-  #   associated_against: {
-  #     subcommunities: [:name]
-  #   },
-  #   using: {
-  #     tsearch: { prefix: true }
-  #   }
+  has_many :interests, as: :interestable, dependent: :destroy
+  has_one :groupchat, as: :groupchatable, dependent: :destroy
 end
