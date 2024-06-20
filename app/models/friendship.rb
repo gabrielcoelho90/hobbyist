9654f3f97b149a3eb191ab9c81cfc268447eb2da
Gabriel Chein <gabrielbcmuniz@gmail.com>
2024-06-20 14:42:31 -0300

    Reflected the PrivateChat validations on the Friendships model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,23 @@ class Friendship < ApplicationRecord
 
   belongs_to :asker, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  validate :unique_friend_pair, on: :create
+
+  validates :asker, comparison: { other_than: :receiver, message: "cannot create friendship with self" }
+
+  def self.find_friendship(options = {})
+    friendship_arr = Friendship.where(asker: options[:lookup_asker], receiver: options[:lookup_receiver]) |
+                     Friendship.where(asker: options[:lookup_receiver], receiver: options[:lookup_asker])
+
+    friendship_arr.first
+  end
+
+  private
+
+  def unique_friend_pair
+    if Friendship.exists?(asker: asker, receiver: receiver) || Friendship.exists?(asker: receiver, receiver: asker)
+      errors.add(:base, "Friendship between these users already exists")
+    end
+  end
 end
